a610f5ccc670f1fb4decc8d02de1e19cce366c08
EliseoCasasBossicovich <beliseocasas@gmail.com>
2022-09-22 15:03:49 -0300

    agregado de forma a pagina contactanos.html

---
+++ form.js
@@ -0,0 +1,19 @@
+const $form = document.querySelector("#form");
+
+$form.addEventListener ("sumbit", handleSumbit);
+
+async function handleSumbit(event){
+    event.preventDefault();
+    const form = new FormData(this);
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+    if (response.ok){
+        this.reset();
+        alert ("Gracias por contactarnos, te escribiremos pronto.");
+    }
+}
