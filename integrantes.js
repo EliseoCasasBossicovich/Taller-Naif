006de6498351cc337bc3e69f2df08d7a49cbc545
EliseoCasasBossicovich <beliseocasas@gmail.com>
2022-09-29 11:29:59 -0300

    Correccion de errror en nav en index y agregado de integrantes.js

---
+++ integrantes.js
@@ -0,0 +1,28 @@
+// Clases
+// Integrantes
+class Integrantes {
+    constructor (nombre, frase, imagen1, imagen2, imagen3){
+        this.nombre = nombre;
+        this.frese = frase;
+        this.imagen1 = imagen1;
+        this.imagen2 = imagen2;
+        this.imagen3 = imagen3;
+    }
+}
+const integrante1 = new Integrantes ("Patricia A. Bossicovich", "Frase1",src = "recursos/historia.jpg", src = "recursos/historia.jpg", src = "recursos/historia.jpg");
+const integrante2 = new Integrantes ("Bibiana Bossicovich", "Frase2", src = "recursos/historia.jpg", src = "recursos/historia.jpg", src = "recursos/historia.jpg");
+const integrante3 = new Integrantes ("Gabriela Ulivieri","Frase3", src = "recursos/historia.jpg", src = "recursos/historia.jpg", src = "recursos/historia.jpg");
+const integrante4 = new Integrantes ("Karen Liljesthron", "Frase4", src = "recursos/historia.jpg", src = "recursos/historia.jpg", src = "recursos/historia.jpg");
+const integrante5 = new Integrantes ("Gabriela De Dominicis", "Frase5", src = "recursos/historia.jpg", src = "recursos/historia.jpg", src = "recursos/historia.jpg");
+const integrante6 = new Integrantes (" Ingrid Malanot","Frase6", src = "recursos/historia.jpg", src = "recursos/historia.jpg",src = "recursos/historia.jpg");
+
+// Arrays
+// Integrantes
+const integrantes = [];
+
+integrantes.push(integrante1);
+integrantes.push(integrante2);
+integrantes.push(integrante3);
+integrantes.push(integrante4); 
+integrantes.push(integrante5);
+integrantes.push(integrante6);
